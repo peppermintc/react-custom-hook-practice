b6c4737c77225d35ca234222d31dd41425b2b80e
Hamin <haminjp@naver.com>
2021-03-04 17:27:51 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import useLocalStorage from './useLocalStorage';
+import useUpdateLogger from './useUpdateLogger';
 
-function App() {
+export default function App() {
+  const [name, setName] = useLocalStorage('name', '');
+  useUpdateLogger(name);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <input 
+      type="text" 
+      value={name} 
+      onChange={e => setName(e.target.value)} 
+    />
   );
 }
-
-export default App;
